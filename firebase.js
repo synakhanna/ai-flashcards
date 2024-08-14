5c7e5646600ade8162054ec5a1e75c17de18b250
Tahmidur_Rabb <tr1476@nyu.edu>
2024-08-14 19:57:49 -0400

    firebase file

--- firebase.js
+++ firebase.js
@@ -1,13 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD1jXOVYC6PPrLPzAN52_5BG9mMPnWpm0M",
+  authDomain: "ai-code-smart.firebaseapp.com",
+  projectId: "ai-code-smart",
+  storageBucket: "ai-code-smart.appspot.com",
+  messagingSenderId: "567344882182",
+  appId: "1:567344882182:web:1f3e8742ccca6361c61987",
+  measurementId: "G-4LGLDC97Z7"
 };
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-export default db;
+const analytics = getAnalytics(app);
