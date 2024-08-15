ac467d62ee80de56905d9e7b794baaef784dce77
synakhanna <sk9921@nyu.edu>
2024-08-15 14:36:03 -0700

    homepage responsive

--- app/Code/page.js
+++ app/Code/page.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './code.module.css';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import FlashCardMenu from '../components/flashCardMenu';
 import FlashCardNav from '../components/flashCardNav';
+import styles from './code.module.css';
 
 export default function Home() {
   return (
--- app/Home/page.js
+++ app/Home/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
         <Header />
-        
+
         <FlashCardMenu />
 
       {/* Welcome Section */}
