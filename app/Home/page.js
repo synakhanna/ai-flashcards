a3a7ad0a40f2dc15d3f6c73d9af8f926c7a9257f
synakhanna <sk9921@nyu.edu>
2024-08-14 22:07:05 -0700

    home page updates

--- app/Home/page.js
+++ app/Home/page.js
@@ -7,12 +7,19 @@ import SettingsIcon from '@mui/icons-material/Settings';
 export default function Home() {
   return (
     <div className={styles.container}>
-      {/* Header Section */}
+   {/* Header Section */}
       <header className={styles.header}>
         <div className={styles.logo}>
         <FlashOnIcon className={styles.logoImage} />
         <h1 className={styles.title}>CodeFlash</h1>
         </div>
+        <div className={styles.actions}>
+        <SettingsIcon style={{ fontSize: 40 , marginRight: '10px'}}/>
+          <Link href="/SignIn">
+            <button className={styles.logoutButton}>Logout</button>
+          </Link>
+        </div>
+        </header>
         <nav className={styles.nav}>
           <a href="#" className={styles.navItem}>HTML</a>
           <a href="#" className={styles.navItem}>CSS</a>
@@ -23,18 +30,16 @@ export default function Home() {
           <a href="#" className={styles.navItem}>C++</a>
           <a href="#" className={styles.navItem}>SQL</a>
         </nav>
-        <div className={styles.actions}>
-          <SettingsIcon style={{ fontSize: 40 }} />
-          <Link href="/SignIn">
-            <button className={styles.logoutButton}>Logout</button>
-          </Link>
-        </div>
-      </header>
 
       {/* Welcome Section */}
       <section className={styles.welcomeSection}>
         <h2 className={styles.welcomeMessage}>Hi, Name. Which subject do you need help in today?</h2>
       </section>
+
+      {/* Footer Section */}
+      <footer className={styles.footer}>
+        <p>&copy; 2024 CodeFlash. All Rights Reserved.</p>
+      </footer>
     </div>
   );
 }
