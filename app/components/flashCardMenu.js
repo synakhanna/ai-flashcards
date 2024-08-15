5f0f660fe08448e895d750701dd6ebf33bcc5966
synakhanna <sk9921@nyu.edu>
2024-08-15 11:28:21 -0700

    fixed up routes in code

--- app/components/flashCardMenu.js
+++ app/components/flashCardMenu.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import styles from './code.module.css';
 
-export default function FlashcardMenu() {
+export default function FlashCardMenu() {
   return (
     <nav className={styles.nav}>
-      <a href="/HTML" className={styles.navItem}>HTML</a>
-      <a href="/CSS" className={styles.navItem}>CSS</a>
-      <a href="/Python" className={styles.navItem}>Python</a>
-      <a href="/JavaScript" className={styles.navItem}>JavaScript</a>
-      <a href="/Java" className={`${styles.navItem} ${styles.selectedItem}`}>Java</a>
-      <a href="/C" className={styles.navItem}>C</a>
-      <a href="/CPlusPlus" className={styles.navItem}>C++</a>
-      <a href="/SQL" className={styles.navItem}>SQL</a>
+      <a href="/Code" className={styles.navItem}>HTML</a>
+      <a href="/Code" className={styles.navItem}>CSS</a>
+      <a href="/Code" className={styles.navItem}>Python</a>
+      <a href="/Code" className={styles.navItem}>JavaScript</a>
+      <a href="/Code" className={styles.navItem}>Java</a>
+      <a href="/Code" className={styles.navItem}>C</a>
+      <a href="/Code" className={styles.navItem}>C++</a>
+      <a href="/Code" className={styles.navItem}>SQL</a>
     </nav>
+
   );
 }
