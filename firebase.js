c7127309f5ab7f70f1ffdadc6c5452de7e51ce83
Fatima Riaz <fatima_gatech@outlook.com>
2024-08-14 21:50:49 -0500

    fixing firebase config

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD1jXOVYC6PPrLPzAN52_5BG9mMPnWpm0M",
-  authDomain: "ai-code-smart.firebaseapp.com",
-  projectId: "ai-code-smart",
-  storageBucket: "ai-code-smart.appspot.com",
-  messagingSenderId: "567344882182",
-  appId: "1:567344882182:web:1f3e8742ccca6361c61987",
-  measurementId: "G-4LGLDC97Z7"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
