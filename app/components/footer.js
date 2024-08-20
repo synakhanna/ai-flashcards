0331fcb87b66f5f4b5de4b7ea36be7e753ae815d
synakhanna <sk9921@nyu.edu>
2024-08-20 16:36:35 -0700

    linkedin logo in footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './components.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2024 CodeFlash. All Rights Reserved.</p>
+      <p>&copy; 2024 CodeFlash. All Rights Reserved.<a href="https://www.linkedin.com/company/codefusionartificialintelligence/" target="_blank"><FontAwesomeIcon className={styles.linkedinLogo} icon={faLinkedin} /></a></p>
     </footer>
   );
 }
